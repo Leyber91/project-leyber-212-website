e5673987a43c6711d4a32312ba801ce38c1c2476
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-03-26 22:34:03 +0100

    Commit 0008, content fix

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -13,9 +13,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function initTypewriter() {
-        mainHeader.textContent = '';
-        mainText.textContent = '';
-
+        mainHeader.textContent = 'Welcome to Project Leyber 212';
+        mainText.textContent = 'Join us on this exciting journey as we explore the endless possibilities of the universe and the human spirit. Our mission is to inspire and empower our audience by sharing valuable insights and knowledge in diverse fields, from space science and fitness to data engineering and artificial intelligence. Together, we\'ll navigate this rapidly changing landscape and foster a community of forward-thinking individuals who embrace progress and seek to make a positive impact in the world.';
         setTimeout(() => typeWriter(mainHeader), 1000);
         setTimeout(() => typeWriter(mainText), 2000);
     }
