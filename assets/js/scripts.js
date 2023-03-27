5101bb689672248bc5debd3ebf3f5255d8271923
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-03-27 05:20:00 +0100

    Commit 0020. Evolving it

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Add the Tryverse section
+    addTryverseSection();
+    addParallaxEffectToTitle();
+    initializeGSAPAnimations();
+    addSidebarBlockHoverEffects();
+});
+
+function addTryverseSection() {
     const tryverseSection = document.createElement('section');
     tryverseSection.classList.add('tryverse');
     tryverseSection.innerHTML = `
         <h2>The Tryverse</h2>
         <p>Enter the Tryverse, where we explore alternative realities based on the events of the current timeline. Experience two unique dimensions: one where reality is even better than it is now, and another where everything takes a darker turn. Let's dive into these fascinating worlds and discover how small events can drastically change the course of history.</p>
     `;
-    
     document.querySelector('main').appendChild(tryverseSection);
-  });
+}
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Toggle sidebar on click
-    const menuToggle = document.querySelector('.menu-toggle');
-    const sidebar = document.querySelector('#sidebar');
-
-    menuToggle.addEventListener('click', function () {
-        sidebar.classList.toggle('show');
-    });
-
-    // Add parallax effect to the title
+function addParallaxEffectToTitle() {
     const title = document.querySelector('.title');
 
     window.addEventListener('scroll', function () {
         let scrollPosition = window.pageYOffset;
         title.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
     });
-});
+}
+
+function initializeGSAPAnimations() {
+    const tl = gsap.timeline({ defaults: { duration: 1, ease: 'power2.out' } });
+
+    tl.from('.logo, .nav-links li a', { opacity: 0, y: '-100%', stagger: 0.2 });
+    tl.from('#sidebar', { x: '-100%', duration: 0.5 });
+    tl.from('.sidebar-block', { opacity: 0, y: '-50%', stagger: 0.2, duration: 0.5 });
+    tl.from('main', { opacity: 0, x: '100%', duration: 0.5 });
+}
+
+function addSidebarBlockHoverEffects() {
+    const sidebarBlocks = document.querySelectorAll('.sidebar-block');
+
+    sidebarBlocks.forEach((block) => {
+        block.addEventListener('mouseover', () => {
+            gsap.to(block, { scale: 1.1, duration: 0.3, ease: 'power2.out' });
+            gsap.to(block.nextElementSibling, { scale: 0.9, duration: 0.3, ease: 'power2.out' });
+        });
+
+        block.addEventListener('mouseout', () => {
+            gsap.to(block, { scale: 1, duration: 0.3, ease: 'power2.out' });
+            gsap.to(block.nextElementSibling, { scale: 1, duration: 0.3, ease: 'power2.out' });
+        });
+    });
+}
