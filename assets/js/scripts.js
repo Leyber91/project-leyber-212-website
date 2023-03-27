65382589c1520876ca83b167f5677ea9e513988a
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-03-27 04:56:06 +0100

    Commit 0018, getting back in track gpt-4

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,4 +9,33 @@ document.addEventListener('DOMContentLoaded', () => {
     
     document.querySelector('main').appendChild(tryverseSection);
   });
-  
+
+// GSAP Animations
+const tl = gsap.timeline({ defaults: { duration: 1, ease: 'power2.out' } });
+
+// Logo and navigation links animation
+tl.from('.logo, .nav-links li a', { opacity: 0, y: '-100%', stagger: 0.2 });
+
+// Sidebar animation
+tl.from('#sidebar', { x: '-100%', duration: 0.5 });
+
+// Sidebar blocks animation
+tl.from('.sidebar-block', { opacity: 0, y: '-50%', stagger: 0.2, duration: 0.5 });
+
+// Main content animation
+tl.from('main', { opacity: 0, x: '100%', duration: 0.5 });
+
+// Hover effect on sidebar blocks
+const sidebarBlocks = document.querySelectorAll('.sidebar-block');
+
+sidebarBlocks.forEach((block) => {
+  block.addEventListener('mouseover', () => {
+    gsap.to(block, { scale: 1.1, duration: 0.3, ease: 'power2.out' });
+    gsap.to(block.nextElementSibling, { scale: 0.9, duration: 0.3, ease: 'power2.out' });
+  });
+
+  block.addEventListener('mouseout', () => {
+    gsap.to(block, { scale: 1, duration: 0.3, ease: 'power2.out' });
+    gsap.to(block.nextElementSibling, { scale: 1, duration: 0.3, ease: 'power2.out' });
+  });
+});
