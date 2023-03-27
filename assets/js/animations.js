a55da228a207efb16a2ecc3b0a0851fbd2c186db
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-03-27 01:06:49 +0100

    Commit 0011, New face

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -20,26 +20,50 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     initTypewriter();
-    
-    const countdown = document.querySelector('.countdown');
-    const launchDate = new Date('Jan 1, 2024 00:00:00').getTime();
 
-    function updateCountdown() {
-        const now = new Date().getTime();
-        const distance = launchDate - now;
+    const navLinks = document.querySelectorAll('nav a');
+    const sections = document.querySelectorAll('section');
 
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        countdown.innerHTML = `
-            <div>${days}<span>Days</span></div>
-            <div>${hours}<span>Hours</span></div>
-            <div>${minutes}<span>Minutes</span></div>
-            <div>${seconds}<span>Seconds</span></div>
-        `;
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
     }
 
-    setInterval(updateCountdown, 1000);
+    function updateActiveNavLink() {
+        navLinks.forEach(link => {
+            const href = link.getAttribute('href').replace('#', '');
+            const section = document.getElementById(href);
+            if (isInViewport(section)) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', updateActiveNavLink);
+
+    const logo = document.querySelector('header h1');
+    logo.addEventListener('mouseenter', () => {
+        logo.classList.add('animate__animated', 'animate__bounce');
+    });
+
+    logo.addEventListener('animationend', () => {
+        logo.classList.remove('animate__animated', 'animate__bounce');
+    });
+
+    const introSection = document.querySelector('#intro');
+    const introButton = introSection.querySelector('button');
+    introButton.addEventListener('click', () => {
+        introSection.classList.add('animate__animated', 'animate__zoomOutUp');
+    });
+
+    introSection.addEventListener('animationend', () => {
+        introSection.style.display = 'none';
+    });
 });
