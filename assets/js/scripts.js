b7ed9cc5be0edf99ea736fb8e6cc486e7ca695c9
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-03-28 21:13:22 +0100

    Commit 0053. Black hole. PART III

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -213,86 +213,91 @@ function getRandomPoints() {
 
     return [startPoint, ...randomPoints, endPoint].join(' ');
 }
+function createBlackHoleEffect() {
+    const canvas = document.getElementById('blackHole');
+    const ctx = canvas.getContext('2d');
 
-const canvas = document.getElementById('blackHole');
-const ctx = canvas.getContext('2d');
 
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
 
-const particlesArray = [];
-const particlesCount = 200;
+    const particlesArray = [];
+    const particlesCount = 200;
 
-class Particle {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 5 + 1;
-    this.speedX = Math.random() * 3 - 1.5;
-    this.speedY = Math.random() * 3 - 1.5;
-  }
-
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-
-    if (this.size > 0.2) this.size -= 0.1;
-
-    // Creating black hole effect
-    const dx = canvas.width / 2 - this.x;
-    const dy = canvas.height / 2 - this.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    const forceDirectionX = dx / distance;
-    const forceDirectionY = dy / distance;
-    const maxDistance = canvas.width / 2;
-    const force = (maxDistance - distance) / maxDistance;
-    const directionX = forceDirectionX * force * this.size;
-    const directionY = forceDirectionY * force * this.size;
-      // Check if the particle is within the title area
-    const titleRadius = 100; // Adjust this value to change the size of the title area
-    const dxTitle = canvas.width / 2 - this.x;
-    const dyTitle = canvas.height / 2 - this.y;
-    const distanceTitle = Math.sqrt(dxTitle * dxTitle + dyTitle * dyTitle);
-
-    if (distanceTitle < titleRadius) {
-        this.x -= directionX * 1.5;
-        this.y -= directionY * 1.5;
-    } else {
-      this.x -= this.speedX;
-      this.y -= this.speedY;
+    class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
     }
-  }
 
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.closePath();
-    ctx.fillStyle = 'rgba(58, 123, 213,' + this.size / 5 + ')';
-    ctx.fill();
-  }
-}
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
 
-function init() {
-  for (let i = 0; i < particlesCount; i++) {
-    particlesArray.push(new Particle());
-  }
-}
+        if (this.size > 0.2) this.size -= 0.1;
 
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // Creating black hole effect
+        const dx = canvas.width / 2 - this.x;
+        const dy = canvas.height / 2 - this.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const forceDirectionX = dx / distance;
+        const forceDirectionY = dy / distance;
+        const maxDistance = canvas.width / 2;
+        const force = (maxDistance - distance) / maxDistance;
+        const directionX = forceDirectionX * force * this.size;
+        const directionY = forceDirectionY * force * this.size;
+        // Check if the particle is within the title area
+        const titleRadius = 100; // Adjust this value to change the size of the title area
+        const dxTitle = canvas.width / 2 - this.x;
+        const dyTitle = canvas.height / 2 - this.y;
+        const distanceTitle = Math.sqrt(dxTitle * dxTitle + dyTitle * dyTitle);
 
-  for (let i = 0; i < particlesArray.length; i++) {
-    particlesArray[i].update();
-    particlesArray[i].draw();
-
-    if (particlesArray[i].size <= 0.2) {
-      particlesArray.splice(i, 1);
-      i--;
+        if (distanceTitle < titleRadius) {
+            this.x -= directionX * 1.5;
+            this.y -= directionY * 1.5;
+        } else {
+        this.x -= this.speedX;
+        this.y -= this.speedY;
+        }
     }
-  }
 
-  requestAnimationFrame(animate);
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fillStyle = 'rgba(58, 123, 213,' + this.size / 5 + ')';
+        ctx.fill();
+    }
+    }
+
+    function init() {
+    for (let i = 0; i < particlesCount; i++) {
+        particlesArray.push(new Particle());
+    }
+    }
+
+    function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+
+        if (particlesArray[i].size <= 0.2) {
+        particlesArray.splice(i, 1);
+        i--;
+        }
+    }
+
+    requestAnimationFrame(animate);
+    }
+
+    init();
+    animate();
+
 }
 
-init();
-animate();
+window.addEventListener('DOMContentLoaded', createBlackHoleEffect);
