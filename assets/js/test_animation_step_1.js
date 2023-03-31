65f0aa3189d390238a770827951e59e9f1e8edaa
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-03-31 15:53:36 +0100

    Commit 0106. Black hole project step back. Step 1. p5 apprach. Debugging XVI

--- assets/js/test_animation_step_1.js
+++ assets/js/test_animation_step_1.js
@@ -1,74 +1,69 @@
-window.onload = function() {
-  function initialize() {
-    let sphere;
-    let colorPicker;
-    let rangeSpeed;
-    let rangeSize;
-    let rangeRotationX;
-    let rangeRotationY;
-    let rangeRotationZ;
-    let isAnimating = true;
+function initialize() {
+  let sphere;
+  let colorPicker;
+  let rangeSpeed;
+  let rangeSize;
+  let rangeRotationX;
+  let rangeRotationY;
+  let rangeRotationZ;
+  let isAnimating = true;
 
-    function setup() {
-      let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-      canvas.parent('animation-container');
+  function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    canvas.parent('animation-container');
 
-      sphere = createSphere(50);
+    sphere = createSphere(50);
 
-      colorPicker = select('#colorPicker');
+    colorPicker = select('#colorPicker');
 
-      rangeSpeed = select('#rangeSpeed');
+    rangeSpeed = select('#rangeSpeed');
 
-      rangeSize = select('#rangeSize');
+    rangeSize = select('#rangeSize');
 
-      rangeRotationX = select('#rangeRotationX');
+    rangeRotationX = select('#rangeRotationX');
 
-      rangeRotationY = select('#rangeRotationY');
+    rangeRotationY = select('#rangeRotationY');
 
-      rangeRotationZ = select('#rangeRotationZ');
+    rangeRotationZ = select('#rangeRotationZ');
 
-      const toggleAnimationButton = select('#toggleAnimation');
-      toggleAnimationButton.mousePressed(function (event) {
-        isAnimating = !isAnimating;
-      });
-    }
-
-    function draw() {
-      background(50);
-
-      let size = rangeSize.value();
-      let angleX = radians(rangeRotationX.value());
-      let angleY = radians(rangeRotationY.value());
-      let angleZ = radians(rangeRotationZ.value());
-      let speed = rangeSpeed.value();
-
-      push();
-      if (isAnimating) {
-        rotateX(angleX + frameCount * speed * 0.01);
-        rotateY(angleY + frameCount * speed * 0.01);
-        rotateZ(angleZ + frameCount * speed * 0.01);
-      } else {
-        rotateX(angleX);
-        rotateY(angleY);
-        rotateZ(angleZ);
-      }
-      scale(size / 50);
-      fill(colorPicker.color());
-      sphere();
-      pop();
-    }
-
-    function createSphere(radius) {
-      const sphere = () => {
-        ellipsoid(radius, radius, radius);
-      };
-      return sphere;
-    }
-
+    const toggleAnimationButton = select('#toggleAnimation');
+    toggleAnimationButton.mousePressed(function (event) {
+      isAnimating = !isAnimating;
+    });
   }
 
+  function draw() {
+    background(50);
 
-};
+    let size = rangeSize.value();
+    let angleX = radians(rangeRotationX.value());
+    let angleY = radians(rangeRotationY.value());
+    let angleZ = radians(rangeRotationZ.value());
+    let speed = rangeSpeed.value();
+
+    push();
+    if (isAnimating) {
+      rotateX(angleX + frameCount * speed * 0.01);
+      rotateY(angleY + frameCount * speed * 0.01);
+      rotateZ(angleZ + frameCount * speed * 0.01);
+    } else {
+      rotateX(angleX);
+      rotateY(angleY);
+      rotateZ(angleZ);
+    }
+    scale(size / 50);
+    fill(colorPicker.color());
+    sphere();
+    pop();
+  }
+
+  function createSphere(radius) {
+    const sphere = () => {
+      ellipsoid(radius, radius, radius);
+    };
+    return sphere;
+  }
+}
 
 window.onload = function() {
   new p5(initialize);
