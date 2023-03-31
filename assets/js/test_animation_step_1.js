4e4c9bc42fe7b49ba1dacb09701cf33ebc2539f6
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-03-31 14:23:02 +0100

    Commit 0100. Black hole project step back. Step 1. p5 apprach. Debugging VIII

--- assets/js/test_animation_step_1.js
+++ assets/js/test_animation_step_1.js
@@ -43,41 +43,31 @@ function setup() {
 }
 
 function draw() {
-  console.log('Drawing the sketch');
   background(50);
 
   let size = rangeSize.value();
-  console.log('Size value:', size);
   let angleX = radians(rangeRotationX.value());
-  console.log('Angle X value:', angleX);
   let angleY = radians(rangeRotationY.value());
-  console.log('Angle Y value:', angleY);
   let angleZ = radians(rangeRotationZ.value());
-  console.log('Angle Z value:', angleZ);
   let speed = rangeSpeed.value();
-  console.log('Speed value:', speed);
 
   push();
   if (isAnimating) {
-    console.log('Animating the sphere');
     rotateX(angleX + frameCount * speed * 0.01);
     rotateY(angleY + frameCount * speed * 0.01);
     rotateZ(angleZ + frameCount * speed * 0.01);
   } else {
-    console.log('Not animating the sphere');
     rotateX(angleX);
     rotateY(angleY);
     rotateZ(angleZ);
   }
   scale(size / 50);
   fill(colorPicker.color());
-  console.log('Drawing the sphere');
   sphere();
   pop();
 }
 
 function createSphere(radius) {
-  console.log('Creating the sphere function');
   const sphere = () => {
     ellipsoid(radius, radius, radius);
   };
