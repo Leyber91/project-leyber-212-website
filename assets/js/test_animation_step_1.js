3ea7054140c2c5ef0c044c1007eff2e0135f5d5e
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-03-31 13:51:08 +0100

    Commit 0098. Black hole project step back. Step 1. p5 apprach. Debugging

--- assets/js/test_animation_step_1.js
+++ assets/js/test_animation_step_1.js
@@ -8,60 +8,76 @@ let rangeRotationZ;
 let isAnimating = true;
 
 function setup() {
+  console.log('Setting up the sketch');
+  console.log('p5.js version:', p5.prototype.VERSION);
   createCanvas(windowWidth, windowHeight, WEBGL);
 
   sphere = createSphere(50);
 
   colorPicker = select('#colorPicker');
+  console.log('Color picker:', colorPicker);
 
   rangeSpeed = select('#rangeSpeed');
+  console.log('Speed slider:', rangeSpeed);
 
   rangeSize = select('#rangeSize');
+  console.log('Size slider:', rangeSize);
 
   rangeRotationX = select('#rangeRotationX');
+  console.log('Rotation X slider:', rangeRotationX);
 
   rangeRotationY = select('#rangeRotationY');
+  console.log('Rotation Y slider:', rangeRotationY);
 
   rangeRotationZ = select('#rangeRotationZ');
+  console.log('Rotation Z slider:', rangeRotationZ);
 
   const toggleAnimationButton = select('#toggleAnimation');
+  console.log('Toggle animation button:', toggleAnimationButton);
   toggleAnimationButton.mousePressed(function (event) {
     isAnimating = !isAnimating;
+    console.log('Toggle animation button pressed, isAnimating:', isAnimating);
   });
 }
 
 function draw() {
+  console.log('Drawing the sketch');
   background(50);
 
   let size = rangeSize.value();
+  console.log('Size value:', size);
   let angleX = radians(rangeRotationX.value());
+  console.log('Angle X value:', angleX);
   let angleY = radians(rangeRotationY.value());
+  console.log('Angle Y value:', angleY);
   let angleZ = radians(rangeRotationZ.value());
+  console.log('Angle Z value:', angleZ);
   let speed = rangeSpeed.value();
+  console.log('Speed value:', speed);
 
   push();
   if (isAnimating) {
+    console.log('Animating the sphere');
     rotateX(angleX + frameCount * speed * 0.01);
     rotateY(angleY + frameCount * speed * 0.01);
     rotateZ(angleZ + frameCount * speed * 0.01);
   } else {
+    console.log('Not animating the sphere');
     rotateX(angleX);
     rotateY(angleY);
     rotateZ(angleZ);
   }
   scale(size / 50);
   fill(colorPicker.color());
+  console.log('Drawing the sphere');
   sphere();
   pop();
 }
 
 function createSphere(radius) {
+  console.log('Creating the sphere function');
   const sphere = () => {
     ellipsoid(radius, radius, radius);
   };
   return sphere;
 }
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  new p5();
-});
