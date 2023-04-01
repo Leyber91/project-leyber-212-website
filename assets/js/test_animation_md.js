e8c15635b1b78163ca58a1363fb292857b07f9d5
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-02 00:21:57 +0100

    Commit 0140. The DImensional Shape. First prototype: Theseract V. Quick fix VI

--- assets/js/test_animation_md.js
+++ assets/js/test_animation_md.js
@@ -1,7 +1,3 @@
-import * as THREE from './libraries/Three.js';
-import { mergeBufferGeometries} from './libraries/BufferGeometryUtils.js';
-
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
