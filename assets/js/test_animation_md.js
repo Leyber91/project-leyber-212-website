58e3427c73ab06cd2aef5515651c0936d87b4380
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-01 15:41:36 +0100

    Commit 0134. The DImensional Shape. First prototype: VIII

--- assets/js/test_animation_md.js
+++ assets/js/test_animation_md.js
@@ -1,129 +1,106 @@
-// Utility functions for matrix operations
-function createIdentityMatrix(dimension) {
-    const matrix = [];
-    for (let i = 0; i < dimension; i++) {
-      matrix[i] = [];
-      for (let j = 0; j < dimension; j++) {
-        matrix[i][j] = i === j ? 1 : 0;
-      }
-    }
-    return matrix;
-  }
-  
-  function applyMatrix(vec, matrix) {
-    const result = new THREE.Vector3();
-    result.x = vec.x * matrix[0][0] + vec.y * matrix[0][1] + vec.z * matrix[0][2];
-    result.y = vec.x * matrix[1][0] + vec.y * matrix[1][1] + vec.z * matrix[1][2];
-    result.z = vec.x * matrix[2][0] + vec.y * matrix[2][1] + vec.z * matrix[2][2];
-    return result;
-  }
-  
-  function rotateAroundAxis(axis, angle) {
-    const c = Math.cos(angle);
-    const s = Math.sin(angle);
-    const t = 1 - c;
-    const x = axis.x;
-    const y = axis.y;
-    const z = axis.z;
-  
-    return [
-      [t * x * x + c, t * x * y - z * s, t * x * z + y * s],
-      [t * x * y + z * s, t * y * y + c, t * y * z - x * s],
-      [t * x * z - y * s, t * y * z + x * s, t * z * z + c],
-    ];
-  }
-  
-  function projectToHigherDimension(vertices, dimension) {
-    const rotationAngle = 0.01;
-    const rotationAxis = new THREE.Vector3(1, 1, 1).normalize();
-    const rotationMatrix = rotateAroundAxis(rotationAxis, rotationAngle);
-  
-    return vertices.map(vertex => {
-      let projectedVertex = vertex.clone();
-      for (let i = 0; i < dimension - 3; i++) {
-        projectedVertex = applyMatrix(projectedVertex, rotationMatrix);
-      }
-      return projectedVertex;
-    });
-  }
-  
-  // Scene setup
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("animation") });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000, 0.8);
+
+const parentObject = new THREE.Object3D();
+
+const cubeGeometry = new THREE.BoxGeometry();
+
+const cube = new THREE.LineSegments(
+  new THREE.EdgesGeometry(cubeGeometry),
+  new THREE.LineBasicMaterial({ color: 0xffffff })
+);
+cube.edgesGeometry = cube.geometry;
+parentObject.add(cube);
+
+scene.add(parentObject);
+
+camera.position.z = 5;
+
+let isAnimating = true;
+let scale = 1;
+let speed = 0.01;
+let directionX = 0;
+let directionY = 0;
+let directionZ = 0;
+
+document.getElementById("toggleAnimation").addEventListener("click", () => {
+  isAnimating = !isAnimating;
+});
+
+document.getElementById("rangeSize").addEventListener("input", (e) => {
+  scale = e.target.value / 50;
+});
+
+document.getElementById("rangeSpeed").addEventListener("input", (e) => {
+  speed = e.target.value * 0.01;
+});
+
+document.getElementById("rangeDirectionX").addEventListener("input", (e) => {
+  directionX = e.target.value * 0.01;
+});
+
+document.getElementById("rangeDirectionY").addEventListener("input", (e) => {
+  directionY = e.target.value * 0.01;
+});
+
+document.getElementById("rangeDirectionZ").addEventListener("input", (e) => {
+  directionZ = e.target.value * 0.01;
+});
+
+const dimensionSelector = document.getElementById("dimensionSelector");
+
+function resetCubeGeometry() {
+  const geometry = new THREE.BoxGeometry();
+  cube.geometry.dispose();
+  cube.geometry = new THREE.EdgesGeometry(geometry);
+}
+
+dimensionSelector.addEventListener("change", (e) => {
+  resetCubeGeometry();
+  const selectedDimension = parseInt(e.target.value);
+  const matrix = new THREE.Matrix4();
+  matrix.set(
+    1, 0, 0, 0,
+    0, selectedDimension > 1 ? 1 : 0, 0, 0,
+    0, 0, selectedDimension > 2 ? 1 : 0, 0,
+    0, 0, 0, 1
   );
-  
-  const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("animation") });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0x000000, 0.8);
-  const canvas = document.getElementById("animation");
-  canvas.removeAttribute("width");
-  canvas.removeAttribute("height");
-  
-  const parentObject = new THREE.Object3D();
-  
-  const cubeGeometry = new THREE.BoxGeometry();
-  const cubeEdges = new THREE.EdgesGeometry(cubeGeometry);
-  const cube = new THREE.LineSegments(cubeEdges, new THREE.LineBasicMaterial({ color: 0xffffff }));
-  parentObject.add(cube);
-  
-  scene.add(parentObject);
-  
-  camera.position.z = 5;
-  
-  let isAnimating = true;
-  let scale = 1;
-  let speed = 0.01;
-  let directionX = 0;
-  let directionY = 0;
-  let directionZ = 0;
-  
-  document.getElementById("toggleAnimation").addEventListener("click", () => {
-    isAnimating = !isAnimating;
-  });
-  
-  document.getElementById("rangeSize").addEventListener("input", (e) => {
-    scale = e.target.value / 50;
-  });
-  
-  document.getElementById("rangeSpeed").addEventListener("input", (e) => {
-    speed = e.target.value * 0.01;
-  });
-  
-  document.getElementById("rangeDirectionX").addEventListener("input", (e) => {
-    directionX = e.target.value * 0.01;
-  });
-  
-  document.getElementById("rangeDirectionY").addEventListener("input", (e) => {
-    directionY = e.target.value * 0.01;
-  });
-  
-  document.getElementById("rangeDirectionZ").addEventListener("input", (e) => {
-    directionZ = e.target.value * 0.01;
-  });
-  
-  const dimensionSelector = document.getElementById("dimensionSelector");
-  
-  dimensionSelector.addEventListener("change", () => {
-    cube.geometry = new THREE.BoxGeometry();
-    cube.geometry.computeBoundingSphere();
-  });
-  
-  const animate = function () {
-    requestAnimationFrame(animate);
-  
-    if (isAnimating) {
-      const selectedDimension = parseInt(dimensionSelector.value);
-      if (selectedDimension > 3) {
-        const projectedVertices = projectToHigherDimension(cube.geometry.vertices, selectedDimension);
-        for (let i = 0; i < cube.geometry.vertices.length; i++) {
-          cube.geometry.vertices[i].copy(projectedVertices[i]);
-        }
+  cube.geometry.applyMatrix4(matrix);
+  cube.edgesGeometry.dispose();
+  cube.edgesGeometry = new THREE.EdgesGeometry(cube.geometry);
+});
+
+function projectToHigherDimension(vertices, dimension) {
+  const angle = 0.01;
+  const rotationMatrix = rotateMatrix(dimension, angle);
+  const rotationMatrix3D = new THREE.Matrix4();
+  rotationMatrix3D.fromArray(rotationMatrix);
+
+  const newVertices = [];
+  for (let i = 0; i < vertices.length; i++) {
+    const vertex = vertices[i];
+    const vertex4D = new THREE.Vector4(vertex.x, vertex.y, vertex.z, 0);
+    const rotatedVertex = vertex4D.applyMatrix4(rotationMatrix3D);
+    newVertices.push(new THREE.Vector3(rotatedVertex.x, rotatedVertex.y, rotatedVertex.z));
+  }
+  return newVertices;
+}
+
+const animate = function () {
+  requestAnimationFrame(animate);
+
+  if (isAnimating) {
+    const selectedDimension = parseInt(dimensionSelector.value);
+    if (selectedDimension > 3) {
+        const newVertices = projectToHigherDimension(cube.geometry.vertices, selectedDimension);
+        cube.geometry.vertices = newVertices;
         cube.geometry.verticesNeedUpdate = true;
         cube.geometry.computeBoundingSphere();
+        
       }
       parentObject.rotation.x += speed * directionX;
       parentObject.rotation.y += speed * directionY;
@@ -148,4 +125,26 @@ function createIdentityMatrix(dimension) {
   window.addEventListener("resize", onWindowResize, false);
   onWindowResize();
   
+  function identityMatrix(dimension) {
+    const matrix = [];
+    for (let i = 0; i < dimension; i++) {
+      matrix[i] = [];
+      for (let j = 0; j < dimension; j++) {
+        matrix[i][j] = i === j ? 1 : 0;
+      }
+    }
+    return matrix;
+  }
   
+  function rotateMatrix(dimension, angle) {
+    const c = Math.cos(angle);
+    const s = Math.sin(angle);
+    const matrix = identityMatrix(dimension);
+    matrix[0][0] = c;
+    matrix[0][dimension - 1] = -s;
+    matrix[dimension - 1][0] = s;
+    matrix[dimension - 1][dimension - 1] = c;
+    return matrix;
+  }
+  
+        
