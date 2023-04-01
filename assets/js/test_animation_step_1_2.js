3c93461e6ea519f772ad2e29d3beb6b0b6cac0d3
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-01 03:40:31 +0100

    Commit 0123. Black hole project. Step 3. Preparations before step 4. II

--- assets/js/test_animation_step_1_2.js
+++ assets/js/test_animation_step_1_2.js
@@ -1,96 +1,104 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+import * as THREE from 'three';
 
-const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("animation") });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000, 0.8); // Add this line
-const canvas = document.getElementById("animation");
-canvas.removeAttribute("width");
-canvas.removeAttribute("height");
+let camera, scene, renderer, geometry, material, mesh, parent, size, speed;
+let animationId;
+let animating = true;
 
-const parentObject = new THREE.Object3D();
-
-// Create cube
-const cubeGeometry = new THREE.BoxGeometry();
-const edges = new THREE.EdgesGeometry(cubeGeometry);
-const cube = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
-
-// Create sphere
-const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-// Add both cube and sphere to the parent object
-parentObject.add(cube);
-parentObject.add(sphere);
-
-// Initially hide the sphere
-sphere.visible = false;
-
-scene.add(parentObject);
-
-camera.position.z = 5;
-
-let isAnimating = true;
-let scale = 1;
-let speed = 0.01;
-
-document.getElementById("toggleAnimation").addEventListener("click", () => {
-  isAnimating = !isAnimating;
-});
-
-document.getElementById("rangeSize").addEventListener("input", (e) => {
-  scale = e.target.value / 50;
-});
-
-document.getElementById("rangeSpeed").addEventListener("input", (e) => {
-  speed = e.target.value * 0.01;
-});
-
-const animate = function () {
-  requestAnimationFrame(animate);
-
-  if (isAnimating) {
-    parentObject.rotation.x += speed;
-    parentObject.rotation.y += speed;
-  }
-
-  parentObject.scale.set(scale, scale, scale);
-
-  renderer.render(scene, camera);
-};
-
+init();
 animate();
 
-const container = document.querySelector('.animation-container');
+function init() {
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.z = 1;
 
-// Get the shape selector element
-const shapeSelector = document.getElementById("shapeSelector");
+  scene = new THREE.Scene();
 
-// Add event listener to handle shape changes
-shapeSelector.addEventListener("change", (e) => {
-  const selectedShape = e.target.value;
+  // Add geometry for each shape
+  const cubeGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  const sphereGeometry = new THREE.SphereGeometry(0.1, 32, 32);
+  const cylinderGeometry = new THREE.CylinderGeometry(0.1, 0.1, 0.2, 32);
+  const torusGeometry = new THREE.TorusGeometry(0.1, 0.03, 16, 100);
+  const octahedronGeometry = new THREE.OctahedronGeometry(0.1, 0);
 
-  if (selectedShape === "cube") {
-    cube.visible = true;
-    sphere.visible = false;
-  } else if (selectedShape === "sphere") {
-    cube.visible = false;
-    sphere.visible = true;
+  material = new THREE.MeshNormalMaterial();
+
+  parent = new THREE.Object3D();
+  scene.add(parent);
+
+  // Create meshes for each shape
+  const cubeMesh = new THREE.Mesh(cubeGeometry, material);
+  const sphereMesh = new THREE.Mesh(sphereGeometry, material);
+  const cylinderMesh = new THREE.Mesh(cylinderGeometry, material);
+  const torusMesh = new THREE.Mesh(torusGeometry, material);
+  const octahedronMesh = new THREE.Mesh(octahedronGeometry, material);
+
+  parent.add(cubeMesh);
+  mesh = cubeMesh;
+
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById('animation') });
+  // Set the initial renderer size and add the clearColor
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x000000, 0.8);
+
+  // Remove the canvas attributes
+  const canvas = document.getElementById("animation");
+  canvas.removeAttribute("width");
+  canvas.removeAttribute("height");
+  document.body.appendChild(renderer.domElement);
+
+  // Controls
+  document.getElementById('shapeSelector').addEventListener('change', (e) => {
+    const shape = e.target.value;
+    parent.remove(mesh);
+
+    if (shape === 'cube') {
+      mesh = cubeMesh;
+    } else if (shape === 'sphere') {
+      mesh = sphereMesh;
+    } else if (shape === 'cylinder') {
+      mesh = cylinderMesh;
+    } else if (shape === 'torus') {
+      mesh = torusMesh;
+    } else if (shape === 'octahedron') {
+      mesh = octahedronMesh;
+    }
+
+    parent.add(mesh);
+  });
+
+  document.getElementById('toggleAnimation').addEventListener('click', () => {
+    animating = !animating;
+    document.querySelector('#toggleAnimation .fas.fa-play').style.display = animating ? 'none' : 'inline-block';
+    document.querySelector('#toggleAnimation .fas.fa-pause').style.display = animating ? 'inline-block' : 'none';
+  });
+
+  document.getElementById('rangeSize').addEventListener('input', (e) => {
+    size = parseFloat(e.target.value);
+    mesh.scale.set(size / 10, size / 10, size / 10);
+  });
+
+  document.getElementById('rangeSpeed').addEventListener('input', (e) => {
+    speed = parseFloat(e.target.value);
+  });
+
+  window.addEventListener('resize', onWindowResize);
+}
+
+function animate() {
+  animationId = requestAnimationFrame(animate);
+
+  if (animating) {
+    mesh.rotation.x += (speed || 5) / 1000;
+    mesh.rotation.y += (speed || 5) / 1000;
   }
-});
+
+  renderer.render(scene, camera);
+}
 
 function onWindowResize() {
-  // Update the camera aspect ratio and the renderer size
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize(container.clientWidth * 0.97, container.clientHeight * 0.97);
-
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 // Add the event listener to call the function when the window is resized
