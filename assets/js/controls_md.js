12f2a3535964dea24f1ec25f5fe82cd56e05a277
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-04 12:27:13 +0100

    Commit 185. Multidimensional shape. Controls removed for each mode

--- assets/js/controls_md.js
+++ assets/js/controls_md.js
@@ -1,68 +1,48 @@
+const modeSelector = document.getElementById("modeSelector");
+const rotationSpeedControl = document.getElementById("rotationSpeedControl");
+const distortionSpeedControl = document.getElementById("distortionSpeedControl");
+const distortionControl = document.getElementById("distortionControl");
 
-function toggleControls() {
-    const controls = document.querySelector('.controls');
-    const isVisible = controls.style.display !== 'none';
-    controls.style.display = isVisible ? 'none' : 'block';
+function toggleControlsByMode() {
+  switch (modeSelector.value) {
+    case "alpha":
+      rotationSpeedControl.style.display = "none";
+      distortionSpeedControl.style.display = "none";
+      distortionControl.style.display = "none";
+      break;
+    case "beta":
+      rotationSpeedControl.style.display = "none";
+      distortionSpeedControl.style.display = "none";
+      distortionControl.style.display = "flex";
+      break;
+    case "gamma":
+      rotationSpeedControl.style.display = "none";
+      distortionSpeedControl.style.display = "flex";
+      distortionControl.style.display = "flex";
+      break;
+    case "delta":
+      rotationSpeedControl.style.display = "flex";
+      distortionSpeedControl.style.display = "flex";
+      distortionControl.style.display = "none";
+      break;
+  }
+
+  loadScriptByMode(modeSelector.value);
 }
 
-// Add this function to controls_md.js
-function loadScript(mode) {
-    const script = document.createElement('script');
-    script.src = `./assets/js/animation_md_${mode}.js`;
-    document.body.appendChild(script);
+function loadScriptByMode(mode) {
+  const oldScript = document.getElementById("animationScript");
+  if (oldScript) {
+    oldScript.remove();
   }
-  
-  // Load the initial mode (alpha) by default
-  loadScript('alpha');
 
-  // (existing code)
+  const script = document.createElement("script");
+  script.src = `./assets/js/animation_md_${mode}.js`;
+  script.id = "animationScript";
+  document.body.appendChild(script);
+}
 
-// Get the mode selector element
-const modeSelector = document.getElementById('modeSelector');
+modeSelector.addEventListener("change", toggleControlsByMode);
 
-// Function to show or hide controls based on the selected mode
-function toggleControlsByMode() {
-    const distortionControl = document.getElementById('rangeDistortion').parentElement;
-    const distortionSpeedControl = document.getElementById('rangeDistortionSpeed').parentElement;
-    const rotationSpeedControl = document.getElementById('rangeRotationSpeed').parentElement;
-    
-    const selectedMode = modeSelector.value;
-    
-    if (selectedMode === 'delta') {
-        distortionControl.style.display = 'inline';
-        distortionSpeedControl.style.display = 'inline';
-        rotationSpeedControl.style.display = 'inline';
-    } else if (selectedMode === 'gamma') {
-        distortionControl.style.display = 'inline';
-        distortionSpeedControl.style.display = 'inline';
-        rotationSpeedControl.style.display = 'none';
-    } else if (selectedMode === 'beta') {
-        distortionControl.style.display = 'inline';
-        distortionSpeedControl.style.display = 'none';
-        rotationSpeedControl.style.display = 'none';
-    } else {
-        distortionControl.style.display = 'none';
-        distortionSpeedControl.style.display = 'none';
-        rotationSpeedControl.style.display = 'none';
-    }
-    }
-
-    // Add event listener for mode selector
-    modeSelector.addEventListener('change', toggleControlsByMode);
-
-    // Call the function initially to set the correct visibility of controls
-    toggleControlsByMode();
-
-    // (existing code)
-
-  
-  // Add an event listener to the mode selector
-  document.getElementById('modeSelector').addEventListener('change', (event) => {
-    const mode = event.target.value;
-    // Remove the existing script from the DOM
-    const oldScript = document.querySelector(`script[src^="./assets/js/animation_md_"]`);
-    if (oldScript) oldScript.remove();
-    // Load the new script based on the selected mode
-    loadScript(mode);
-  });
-  
+// Initialize the page with the default mode
+toggleControlsByMode();
