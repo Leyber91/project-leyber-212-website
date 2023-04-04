5c57cb1f82ea67e84ffb86384600f16c1e5061eb
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-04 12:07:08 +0100

    Commit 185. Multidimensional shape. Stage 1.5 successful ish, now proceeding to leave the animation with modes

---
+++ assets/js/animation_md_beta.js
@@ -0,0 +1,228 @@
+// Global variables
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#animation') });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000, 0.8);
+camera.position.z = 5;
+let scale = 1;
+let speed = 0.01;
+let directionX = 0;
+let directionY = 0;
+let directionZ = 0;
+let isAnimating = true;
+let distortionFactor = 0;
+
+
+
+  function createProjectionMatrix(n) {
+    const matrix = [];
+    const angle = Math.PI / 4;
+  
+    for (let i = 0; i < 3; i++) {
+      const row = [];
+      for (let j = 0; j < n; j++) {
+        if (i === j) {
+          row.push(Math.cos(angle));
+        } else if (i === (j + 1) % n || (i === 0 && j === n - 1)) {
+          row.push(-Math.sin(angle));
+        } else {
+          row.push(0);
+        }
+      }
+      matrix.push(row);
+    }
+    return matrix;
+  }
+///Extra control to distort the projection
+  function distortProjectionMatrix(projectionMatrix, distortionFactor) {
+    const distortedMatrix = projectionMatrix.map(row =>
+      row.map(value => value + distortionFactor * (Math.random() * 2 - 1))
+    );
+    return distortedMatrix;
+  }
+  
+
+  // Generate n-dimensional vertices
+function generateNDimensionalVertices(n, size) {
+  const vertices = [];
+  const numVertices = Math.pow(2, n);
+
+  for (let i = 0; i < numVertices; i++) {
+    const vertex = [];
+    for (let j = 0; j < n; j++) {
+      vertex.push((i & (1 << j)) ? size : -size);
+    }
+    vertices.push(vertex);
+  }
+
+  return vertices;
+}
+
+// Generate adjacency matrix
+function generateNDimensionalAdjacencyMatrix(vertices, n) {
+    const adjacencyMatrix = [];
+
+    for (let i = 0; i < vertices.length; i++) {
+        const row = [];
+        for (let j = 0; j < vertices.length; j++) {
+        if (i === j) {
+            row.push(false);
+        } else {
+            const hammingDistance = vertices[i].reduce(
+            (acc, curr, idx) => acc + (curr !== vertices[j][idx] ? 1 : 0),
+            0
+            );
+            row.push(hammingDistance === 1);
+        }
+        }
+        adjacencyMatrix.push(row);
+    }
+
+    return adjacencyMatrix;
+    }
+
+// Projection function
+function projectNDimensionalTo3D(vertices, projectionMatrix) {
+    return vertices.map(vertex => {
+        const projectedVertex = new THREE.Vector3();
+        for (let i = 0; i < projectionMatrix.length; i++) {
+            const projectedValue = vertex.reduce(
+            (acc, curr, idx) => acc + (curr * projectionMatrix[i][idx]),
+            0
+            );
+            projectedVertex.setComponent(i, projectedValue);
+        }
+        return projectedVertex;
+        });
+    }
+  // Wireframe rendering function
+function createNDimensionalWireframe(vertices, adjacencyMatrix, material) {
+    const geometry = new THREE.BufferGeometry();
+
+    const positionData = [];
+    for (let i = 0; i < adjacencyMatrix.length; i++) {
+        for (let j = 0; j < adjacencyMatrix[i].length; j++) {
+        if (adjacencyMatrix[i][j]) {
+            positionData.push(vertices[i].x, vertices[i].y, vertices[i].z);
+            positionData.push(vertices[j].x, vertices[j].y, vertices[j].z);
+            }
+        }
+    }
+
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positionData, 3));
+    return new THREE.LineSegments(geometry, material);
+    }
+
+  // Interface for selecting dimensions
+    const dimensionSelector = document.querySelector('#dimensionSelector');
+    
+// Move adjacencyMatrix variable to the higher scope
+let adjacencyMatrix;
+
+dimensionSelector.addEventListener('change', () => {
+  const dimension = parseInt(dimensionSelector.value, 10);
+  scale = 3 / dimension;
+
+  // Remove existing wireframes from the scene
+  parentObject.remove(...parentObject.children);
+
+  // Generate the n-dimensional vertices and adjacency matrix
+  const vertices = generateNDimensionalVertices(dimension, 1);
+  adjacencyMatrix = generateNDimensionalAdjacencyMatrix(vertices, dimension);
+
+  // Create the projection matrix and project the n-dimensional vertices to 3D
+  const projectionMatrix = createProjectionMatrix(dimension);
+  
+  // Add distortion to the projection matrix based on the distortionFactor (controlled by the range input)
+  const distortedMatrix = distortProjectionMatrix(projectionMatrix, distortionFactor);
+
+  // Project the n-dimensional vertices to 3D using the distorted projection matrix
+  const projectedVertices = projectNDimensionalTo3D(vertices, distortedMatrix);
+
+  // Generate the wireframe representation and add it to the scene
+  const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+  const wireframe = createNDimensionalWireframe(projectedVertices, adjacencyMatrix, material);
+
+  parentObject.add(wireframe);
+});
+
+      
+// Animation and interaction
+
+
+// Optimize for performance
+
+const parentObject = new THREE.Object3D();
+scene.add(parentObject);
+
+// Scene controls
+document.getElementById("toggleAnimation").addEventListener("click", () => {
+    isAnimating = !isAnimating;
+  });
+  
+  document.getElementById("rangeSize").addEventListener("input", (e) => {
+    scale = e.target.value / 50;
+  });
+  
+  document.getElementById("rangeSpeed").addEventListener("input", (e) => {
+    speed = e.target.value * 0.01;
+  });
+  
+  document.getElementById("rangeDirectionX").addEventListener("input", (e) => {
+    directionX = e.target.value * 0.01;
+  });
+  
+  document.getElementById("rangeDirectionY").addEventListener("input", (e) => {
+    directionY = e.target.value * 0.01;
+  });
+  
+  document.getElementById("rangeDirectionZ").addEventListener("input", (e) => {
+    directionZ = e.target.value * 0.01;
+  });
+
+  document.getElementById("rangeDistortion").addEventListener("input", (e) => {
+    distortionFactor = e.target.value * 0.01;
+  
+    // Update the projection matrix with the new distortion factor
+    const dimension = parseInt(dimensionSelector.value, 10);
+    const vertices = generateNDimensionalVertices(dimension, 1);
+    const projectionMatrix = createProjectionMatrix(dimension);
+    const distortedMatrix = distortProjectionMatrix(projectionMatrix, distortionFactor);
+    const projectedVertices = projectNDimensionalTo3D(vertices, distortedMatrix);
+  
+    // Update the wireframe
+    const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+    const wireframe = createNDimensionalWireframe(projectedVertices, adjacencyMatrix, material);
+  
+    parentObject.remove(...parentObject.children);
+    parentObject.add(wireframe);
+  });
+  
+
+// Consider implementing level of detail (LOD) techniques, spatial partitioning, or selectively rendering only parts of the hypercube that are most relevant to the viewer.
+// ...
+
+const animate = function () {
+    requestAnimationFrame(animate);
+  
+    if (isAnimating) {
+      parentObject.rotation.x += speed * directionX;
+      parentObject.rotation.y += speed * directionY;
+      parentObject.rotation.z += speed * directionZ;
+    }
+  
+    parentObject.scale.set(scale, scale, scale);
+  
+    renderer.render(scene, camera);
+  };
+  
+  animate();
+  
+  const container = document.querySelector('.animation-container');
+  
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.clientWidth * 0.97, container.clientHeight * 0.97);
+  }
--- assets/js/controls_md.js
+++ assets/js/controls_md.js
@@ -4,3 +4,65 @@ function toggleControls() {
     const isVisible = controls.style.display !== 'none';
     controls.style.display = isVisible ? 'none' : 'block';
 }
+
+// Add this function to controls_md.js
+function loadScript(mode) {
+    const script = document.createElement('script');
+    script.src = `./assets/js/animation_md_${mode}.js`;
+    document.body.appendChild(script);
+  }
+  
+  // Load the initial mode (alpha) by default
+  loadScript('alpha');
+
+  // (existing code)
+
+// Get the mode selector element
+const modeSelector = document.getElementById('modeSelector');
+
+// Function to show or hide controls based on the selected mode
+function toggleControlsByMode() {
+    const distortionControl = document.getElementById('rangeDistortion').parentElement;
+    const distortionSpeedControl = document.getElementById('rangeDistortionSpeed').parentElement;
+    const rotationSpeedControl = document.getElementById('rangeRotationSpeed').parentElement;
+    
+    const selectedMode = modeSelector.value;
+    
+    if (selectedMode === 'delta') {
+        distortionControl.style.display = 'inline';
+        distortionSpeedControl.style.display = 'inline';
+        rotationSpeedControl.style.display = 'inline';
+    } else if (selectedMode === 'gamma') {
+        distortionControl.style.display = 'inline';
+        distortionSpeedControl.style.display = 'inline';
+        rotationSpeedControl.style.display = 'none';
+    } else if (selectedMode === 'beta') {
+        distortionControl.style.display = 'inline';
+        distortionSpeedControl.style.display = 'none';
+        rotationSpeedControl.style.display = 'none';
+    } else {
+        distortionControl.style.display = 'none';
+        distortionSpeedControl.style.display = 'none';
+        rotationSpeedControl.style.display = 'none';
+    }
+    }
+
+    // Add event listener for mode selector
+    modeSelector.addEventListener('change', toggleControlsByMode);
+
+    // Call the function initially to set the correct visibility of controls
+    toggleControlsByMode();
+
+    // (existing code)
+
+  
+  // Add an event listener to the mode selector
+  document.getElementById('modeSelector').addEventListener('change', (event) => {
+    const mode = event.target.value;
+    // Remove the existing script from the DOM
+    const oldScript = document.querySelector(`script[src^="./assets/js/animation_md_"]`);
+    if (oldScript) oldScript.remove();
+    // Load the new script based on the selected mode
+    loadScript(mode);
+  });
+  
