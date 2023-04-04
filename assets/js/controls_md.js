1d1ce7c14942470edbe5520bd155e24e5bdd9ac0
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-04 12:42:01 +0100

    Commit 186. Multidimensional shape. Quick fix XX

--- assets/js/controls_md.js
+++ assets/js/controls_md.js
@@ -1,48 +1,52 @@
-const modeSelector = document.getElementById("modeSelector");
-const rotationSpeedControl = document.getElementById("rotationSpeedControl");
-const distortionSpeedControl = document.getElementById("distortionSpeedControl");
-const distortionControl = document.getElementById("distortionControl");
+document.addEventListener("DOMContentLoaded", function() {
+    const modeSelector = document.getElementById("modeSelector");
+    const rotationSpeedControl = document.getElementById("rotationSpeedControl");
+    const distortionSpeedControl = document.getElementById("distortionSpeedControl");
+    const distortionControl = document.getElementById("distortionControl");
 
-function toggleControlsByMode() {
-  switch (modeSelector.value) {
-    case "alpha":
-      rotationSpeedControl.style.display = "none";
-      distortionSpeedControl.style.display = "none";
-      distortionControl.style.display = "none";
-      break;
-    case "beta":
-      rotationSpeedControl.style.display = "none";
-      distortionSpeedControl.style.display = "none";
-      distortionControl.style.display = "flex";
-      break;
-    case "gamma":
-      rotationSpeedControl.style.display = "none";
-      distortionSpeedControl.style.display = "flex";
-      distortionControl.style.display = "flex";
-      break;
-    case "delta":
-      rotationSpeedControl.style.display = "flex";
-      distortionSpeedControl.style.display = "flex";
-      distortionControl.style.display = "none";
-      break;
-  }
+    function toggleControlsByMode() {
+    switch (modeSelector.value) {
+        case "alpha":
+        rotationSpeedControl.style.display = "none";
+        distortionSpeedControl.style.display = "none";
+        distortionControl.style.display = "none";
+        break;
+        case "beta":
+        rotationSpeedControl.style.display = "none";
+        distortionSpeedControl.style.display = "none";
+        distortionControl.style.display = "flex";
+        break;
+        case "gamma":
+        rotationSpeedControl.style.display = "none";
+        distortionSpeedControl.style.display = "flex";
+        distortionControl.style.display = "flex";
+        break;
+        case "delta":
+        rotationSpeedControl.style.display = "flex";
+        distortionSpeedControl.style.display = "flex";
+        distortionControl.style.display = "none";
+        break;
+    }
 
-  loadScriptByMode(modeSelector.value);
-}
+    loadScriptByMode(modeSelector.value);
+    }
 
-function loadScriptByMode(mode) {
-  const oldScript = document.getElementById("animationScript");
-  if (oldScript) {
-    oldScript.remove();
-  }
+    function loadScriptByMode(mode) {
+    const oldScript = document.getElementById("animationScript");
+    if (oldScript) {
+        oldScript.remove();
+    }
 
-  const script = document.createElement("script");
-  script.src = `./assets/js/animation_md_${mode}.js`;
-  script.id = "animationScript";
-  document.body.appendChild(script);
-}
+    const script = document.createElement("script");
+    script.src = `./assets/js/animation_md_${mode}.js`;
+    script.id = "animationScript";
+    document.body.appendChild(script);
+    }
 
-modeSelector.addEventListener("change", toggleControlsByMode);
+    modeSelector.addEventListener("change", toggleControlsByMode);
 
-// Initialize the page with the default mode
-toggleControlsByMode();
+    // Initialize the page with the default mode
+    toggleControlsByMode();
+
+});
+
