ec75b53ad7c4b8e6081e3cff61835daefeb1bbcc
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-06 01:33:01 +0100

    Commit 0195. Exomania. Heroku server established. Fixing the query II

--- assets/js/exomania/exomania_main.js
+++ assets/js/exomania/exomania_main.js
@@ -1,89 +1,47 @@
 async function fetchData() {
-    const [keplerData, nasaExoplanetData] = await Promise.all([
-      fetchKeplerData(),
-      fetchNasaExoplanetData(),
-    ]);
-  
-    return mergeData(keplerData, nasaExoplanetData);
-  }
-  
-  async function fetchKeplerData() {
-    const proxyUrl = 'https://my-cors-proxy-server.herokuapp.com/';
-    const url = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+*+from+ps+where+pl_discmethod=%27Transit%27&format=json';
-    
-    const response = await fetch(proxyUrl + url);
-    const data = await response.json();
-    return data.map(planet => ({
-      pl_name: planet.pl_name,
-      pl_mass: planet.pl_bmassj,
-      pl_radius: planet.pl_radj,
-      pl_orbper: planet.pl_orbper,
-      st_distance: planet.st_dist,
-      pl_temperature: planet.pl_eqt,
-      pl_orbeccen: planet.pl_orbeccen,
-      pl_orbincl: planet.pl_orbincl,
-    }));
-  }
-  
-  
-  async function fetchNasaExoplanetData() {
-    const proxyUrl = 'https://my-cors-proxy-server.herokuapp.com/';
-    const url = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+*+from+ps&format=json';
-    
-    const response = await fetch(proxyUrl + url);
-    const data = await response.json();
-    return data.map(planet => ({
-      pl_name: planet.pl_name,
-      pl_mass: planet.pl_bmassj,
-      pl_radius: planet.pl_radj,
-      pl_orbper: planet.pl_orbper,
-      st_distance: planet.st_dist,
-      pl_temperature: planet.pl_eqt,
-      pl_orbeccen: planet.pl_orbeccen,
-      pl_orbincl: planet.pl_orbincl,
-    }));
-  }
-  
-  function mergeData(keplerData, nasaExoplanetData) {
-    const dataMap = new Map();
-  
-    const mergePlanet = planet => {
-      if (dataMap.has(planet.pl_name)) {
-        const existingPlanet = dataMap.get(planet.pl_name);
-        dataMap.set(planet.pl_name, { ...existingPlanet, ...planet });
-      } else {
-        dataMap.set(planet.pl_name, planet);
-      }
-    };
-  
-    keplerData.forEach(mergePlanet);
-    nasaExoplanetData.forEach(mergePlanet);
-  
-    return Array.from(dataMap.values());
-  }
-  
-  function displayCatalog(data) {
-    const catalogElement = document.getElementById('catalog');
-  
-    data.forEach(planet => {
-      const card = document.createElement('div');
-      card.className = 'exoplanet-card';
-      card.innerHTML = `
-        <h3>${planet.pl_name}</h3>
-        <p>Mass: ${planet.pl_mass} Jupiter Masses</p>
-        <p>Radius: ${planet.pl_radius} Jupiter Radii</p>
-        <p>Orbital Period: ${planet.pl_orbper} days</p>
-        <p>Distance: ${planet.st_distance} parsecs</p>
-        <p>Temperature: ${planet.pl_temperature} K</p>
-        <p>Orbital Eccentricity: ${planet.pl_orbeccen}</p>
-        <p>Orbital Inclination: ${planet.pl_orbincl} degrees</p>
-      `;
-      catalogElement.appendChild(card);
-    });
-  }
-  
-  fetchData().then(data => {
-    console.log(data);
-    displayCatalog(data);
+  const allData = await fetchAllData();
+  return processData(allData);
+}
+
+async function fetchAllData() {
+  const proxyUrl = 'https://my-cors-proxy-server.herokuapp.com/';
+  const url = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,pl_rade,pl_masse,pl_orbper,pl_orbeccen,pl_orbincl+from+ps&format=json';
+
+  const response = await fetch(proxyUrl + url);
+  const data = await response.json();
+  return data;
+}
+
+function processData(data) {
+  return data.map(planet => ({
+    pl_name: planet.pl_name,
+    pl_radius: planet.pl_rade,
+    pl_mass: planet.pl_masse,
+    pl_orbper: planet.pl_orbper,
+    pl_orbeccen: planet.pl_orbeccen,
+    pl_orbincl: planet.pl_orbincl,
+  }));
+}
+
+function displayCatalog(data) {
+  const catalogElement = document.getElementById('catalog');
+
+  data.forEach(planet => {
+    const card = document.createElement('div');
+    card.className = 'exoplanet-card';
+    card.innerHTML = `
+      <h3>${planet.pl_name}</h3>
+      <p>Mass: ${planet.pl_mass} Earth Masses</p>
+      <p>Radius: ${planet.pl_radius} Earth Radii</p>
+      <p>Orbital Period: ${planet.pl_orbper} days</p>
+      <p>Orbital Eccentricity: ${planet.pl_orbeccen}</p>
+      <p>Orbital Inclination: ${planet.pl_orbincl} degrees</p>
+    `;
+    catalogElement.appendChild(card);
   });
-  
+}
+
+fetchAllData().then(data => {
+  console.log(data);
+  displayCatalog(data);
+});
