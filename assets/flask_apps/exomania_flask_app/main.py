7ebae4acba7b7611af521a466b9eee8091ecb859
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-08 14:14:13 +0100

    Commit 0214. Exomania. Setting up the refresher rate. Debugging VI

--- assets/flask_apps/exomania_flask_app/main.py
+++ assets/flask_apps/exomania_flask_app/main.py
@@ -4,6 +4,8 @@ import os
 import requests
 from flask_cors import CORS
 from apscheduler.schedulers.background import BackgroundScheduler
+import xml.etree.ElementTree as ET
+from xmljson import badgerfish as bf
 
 
 app = Flask(__name__)
@@ -202,78 +204,35 @@ def fetch_and_update_exoplanets():
     db.session.commit()
     print("6. Cleared existing exoplanets from the database")
 
-    exoplanets_data = response.json()
+    # Parse XML response and convert it to JSON
+    root = ET.fromstring(response.content)
+    exoplanets_data = bf.data(root)['{http://www.ivoa.net/xml/VOTable/v1.3}VOTABLE']['{http://www.ivoa.net/xml/VOTable/v1.3}RESOURCE']['{http://www.ivoa.net/xml/VOTable/v1.3}TABLE']['{http://www.ivoa.net/xml/VOTable/v1.3}DATA']['{http://www.ivoa.net/xml/VOTable/v1.3}TABLEDATA']['{http://www.ivoa.net/xml/VOTable/v1.3}TR']
+
     print("Exoplanets Data:", exoplanets_data)
     for exoplanet_data in exoplanets_data:
     print("Processing exoplanet data:", exoplanet_data)
-      exoplanet = Exoplanet(name=exoplanet_data["name"],
-                            hostname=exoplanet_data["hostname"],
-                            pl_bmassj=exoplanet_data["pl_bmassj"],
-                            pl_radj=exoplanet_data["pl_radj"],
-                            st_dist=exoplanet_data["st_dist"],
-                            pl_disc=exoplanet_data["pl_disc"],
-                            pl_discmethod=exoplanet_data["pl_discmethod"],
-                            pl_facility=exoplanet_data["pl_facility"],
-                            pl_eqt=exoplanet_data["pl_eqt"],
-                            pl_disc_reflink=exoplanet_data["pl_disc_reflink"],
-                            pl_pelink=exoplanet_data["pl_pelink"],
-                            pl_edelink=exoplanet_data["pl_edelink"],
-                            pl_publ_date=exoplanet_data["pl_publ_date"],
-                            pl_mnum=exoplanet_data["pl_mnum"],
-                            pl_orbsmax=exoplanet_data["pl_orbsmax"],
-                            pl_orbeccen=exoplanet_data["pl_orbeccen"],
-                            pl_orbincl=exoplanet_data["pl_orbincl"],
-                            pl_orbper=exoplanet_data["pl_orbper"],
-                            pl_orblper=exoplanet_data["pl_orblper"],
-                            pl_ratdor=exoplanet_data["pl_ratdor"],
-                            pl_ratdorerr1=exoplanet_data["pl_ratdorerr1"],
-                            pl_ratdorerr2=exoplanet_data["pl_ratdorerr2"],
-                            pl_rvamp=exoplanet_data["pl_rvamp"],
-                            pl_rvamperr1=exoplanet_data["pl_rvamperr1"],
-                            pl_rvamperr2=exoplanet_data["pl_rvamperr2"],
-                            pl_insol=exoplanet_data["pl_insol"],
-                            pl_trandep=exoplanet_data["pl_trandep"],
-                            pl_trandeperr1=exoplanet_data["pl_trandeperr1"],
-                            pl_trandeperr2=exoplanet_data["pl_trandeperr2"],
-                            pl_imppar=exoplanet_data["pl_imppar"],
-                            pl_impparerr1=exoplanet_data["pl_impparerr1"],
-                            pl_impparerr2=exoplanet_data["pl_impparerr2"],
-                            pl_orbsmaxerr1=exoplanet_data["pl_orbsmaxerr1"],
-                            pl_orbsmaxerr2=exoplanet_data["pl_orbsmaxerr2"],
-                            pl_orbeccenerr1=exoplanet_data["pl_orbeccenerr1"],
-                            pl_orbeccenerr2=exoplanet_data["pl_orbeccenerr2"],
-                            pl_orbinclerr1=exoplanet_data["pl_orbinclerr1"],
-                            pl_orbinclerr2=exoplanet_data["pl_orbinclerr2"],
-                            pl_orbpererr1=exoplanet_data["pl_orbpererr1"],
-                            pl_orbpererr2=exoplanet_data["pl_orbpererr2"],
-                            st_teff=exoplanet_data["st_teff"],
-                            st_mass=exoplanet_data["st_mass"],
-                            st_rad=exoplanet_data["st_rad"],
-                            st_lum=exoplanet_data["st_lum"],
-                            st_met=exoplanet_data["st_met"],
-                            st_logg=exoplanet_data["st_logg"],
-                            st_spt=exoplanet_data["st_spt"],
-                            sy_pnum=exoplanet_data["sy_pnum"],
-                            sy_mnum=exoplanet_data["sy_mnum"],
-                            sy_plx=exoplanet_data["sy_plx"],
-                            sy_plxerr1=exoplanet_data["sy_plxerr1"],
-                            sy_plxerr2=exoplanet_data["sy_plxerr2"],
-                            sy_dist=exoplanet_data["sy_dist"],
-                            sy_disterr1=exoplanet_data["sy_disterr1"],
-                            sy_disterr2=exoplanet_data["sy_disterr2"],
-                            sy_gaiamag=exoplanet_data["sy_gaiamag"],
-                            sy_gaiamagerr1=exoplanet_data["sy_gaiamagerr1"],
-                            sy_gaiamagerr2=exoplanet_data["sy_gaiamagerr2"],
-                            sy_vmag=exoplanet_data["sy_vmag"],
-                            sy_vmagerr1=exoplanet_data["sy_vmagerr1"],
-                            sy_vmagerr2=exoplanet_data["sy_vmagerr2"],
-                            pl_dens=exoplanet_data["pl_dens"],
-                            pl_denserr1=exoplanet_data["pl_denserr1"],
-                            pl_denserr2=exoplanet_data["pl_denserr2"])
+      exoplanet_fields = [
+        "name", "hostname", "pl_bmassj", "pl_radj", "st_dist", "pl_disc", "pl_discmethod", "pl_facility",
+        "pl_eqt", "pl_disc_reflink", "pl_pelink", "pl_edelink", "pl_publ_date", "pl_mnum", "pl_orbsmax",
+        "pl_orbeccen", "pl_orbincl", "pl_orbper", "pl_orblper", "pl_ratdor", "pl_ratdorerr1", "pl_ratdorerr2",
+        "pl_rvamp", "pl_rvamperr1", "pl_rvamperr2", "pl_insol", "pl_trandep", "pl_trandeperr1", "pl_trandeperr2",
+        "pl_imppar", "pl_impparerr1", "pl_impparerr2", "pl_orbsmaxerr1", "pl_orbsmaxerr2", "pl_orbeccenerr1",
+        "pl_orbeccenerr2", "pl_orbinclerr1", "pl_orbinclerr2", "pl_orbpererr1", "pl_orbpererr2", "st_teff",
+        "st_mass", "st_rad", "st_lum", "st_met", "st_logg", "st_spt", "sy_pnum", "sy_mnum", "sy_plx",
+        "sy_plxerr1", "sy_plxerr2", "sy_dist", "sy_disterr1", "sy_disterr2", "sy_gaiamag", "sy_gaiamagerr1",
+        "sy_gaiamagerr2", "sy_vmag", "sy_vmagerr1", "sy_vmagerr2", "pl_dens", "pl_denserr1", "pl_denserr2"
+        ]
+
+      exoplanet = Exoplanet(**{field: exoplanet_data[field] for field in exoplanet_fields})
+
 
       db.session.add(exoplanet)
+      db.session.commit()
+      print("Exoplanet added:", exoplanet.name)
+  else:
+      print("Failed to fetch data from NASA API")
 
-    db.session.commit()
+
 
 
 if __name__ == "__main__":
