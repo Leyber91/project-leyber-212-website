40cb5c27375febea0e6f44a584b9cb801bf43e04
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-13 18:13:32 +0100

    Commit 364. Exomania reborn. Facial carousel II

--- assets/js/exomania.js
+++ assets/js/exomania.js
@@ -9,11 +9,17 @@ function populateCarousel(data) {
   const carousel = document.querySelector('.carousel');
   data.forEach(planet => {
     const card = document.createElement('div');
+    card.classList.add('carousel-item');
     card.innerHTML = `
       <h2>${planet.pl_name}</h2>
-      <p>Mass: ${planet.pl_masse} Earth Masses</p>
-      <p>Distance from Earth: ${planet.sy_dist} Parsecs</p>
-      <p>Atmospheric Composition: TBA</p>
+      <p>Host Star: ${planet.hostname}</p>
+      <p>Distance from Earth: ${planet.sy_dist.toFixed(2)} Parsecs</p>
+      <p>Orbital Period: ${planet.pl_orbper.toFixed(2)} Days</p>
+      <p>Semi-Major Axis: ${planet.pl_orbsmax.toFixed(2)} AU</p>
+      <p>Planet Radius: ${planet.pl_rade.toFixed(2)} Earth Radii</p>
+      <p>Planet Mass: ${planet.pl_masse.toFixed(2)} Earth Masses</p>
+      <p>Equilibrium Temperature: ${planet.pl_eqt ? planet.pl_eqt.toFixed(2) : 'N/A'} K</p>
+      <p>Stellar Effective Temperature: ${planet.st_teff.toFixed(2)} K</p>
     `;
     carousel.appendChild(card);
   });
