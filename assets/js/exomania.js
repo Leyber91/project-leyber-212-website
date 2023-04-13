897ff181f6d0e958fc755634017399f00708697f
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-14 00:17:27 +0100

    Commit 369. Exomania reborn. Complete makeover III. Stop_play carousel fix X

--- assets/js/exomania.js
+++ assets/js/exomania.js
@@ -120,26 +120,25 @@ function togglePausePlayButton(pausePlayButton) {
     }
   
     $('.carousel').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: true,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 2,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+            },
           },
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
+      
   
     // Add event listener for the pause/play button
     pausePlayButton.addEventListener('click', () => {
