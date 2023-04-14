6bc6dec641974d4dbdf1783b72d0cb6741b3c103
DESKTOP-R8PF6OQ\Luis Blanco <luis.rodriguez@ms.konicaminolta.com>
2023-04-14 23:17:34 +0100

    Commit 375. Exomania reborn. Reset style. Evolving border-image. Brigtness V2

--- assets/js/exomania.js
+++ assets/js/exomania.js
@@ -80,13 +80,10 @@ function generateCompositionStyle(composition) {
   }
   
   function generateStarBrightnessStyle(starTemperature) {
-    // For the basic version, we will use a simple brightness value
-    // You can replace this with a more sophisticated approach later
-    const brightness = starTemperature ? (starTemperature / 10000) * 100 : 50;
-    return {
-      filter: `brightness(${brightness}%)`,
-    };
+    const opacity = starTemperature ? (starTemperature / 10000) * 0.6 : 0.3;
+    return `background-image: radial-gradient(circle at center, rgba(255, 255, 255, ${opacity}) 0%, rgba(255, 255, 255, 0) 100%);`;
   }
+  
 
   function generateTemperatureBorderStyle(temperature) {
     if (temperature === null) return {};
